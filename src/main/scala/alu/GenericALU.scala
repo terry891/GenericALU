85a6bd499a22aa4231dba01207f68715dd395df4
Tianshu Wang <tw295@duke.edu>
2023-01-18 10:14:31 -0500

    Adding berkley hardfloat to generic ALU

--- src/main/scala/alu/GenericALU.scala
+++ src/main/scala/alu/GenericALU.scala
@@ -5,15 +5,16 @@ package alu
 import chisel3._
 import chisel3.util._
 import chisel3.internal.firrtl.Width
+import hardfloat._
 
 trait GenericNumberType[T] extends Bundle:
-  def :=(b: T): Unit
-  def +(b: T): T
-  def -(b: T): T
-  def *(b: T): T
-  def /(b: T): T
-  def <<(b: T): T
-  def >>(b: T): T
+  def :=(that: T): Unit
+  def +(that: T): T
+  def -(that: T): T
+  def *(that: T): T
+  def /(that: T): T
+  def <<(that: T): T
+  def >>(that: T): T
   def zero: T
 
 
@@ -21,37 +22,39 @@ class UInt(val fixed_width: Width) extends GenericNumberType[UInt]:
   val operand_A = chisel3.UInt(fixed_width)
   val output = Wire(new UInt(fixed_width))
 
-  override def :=(a: UInt): UInt = {
-    operand_A := a
+  override def :=(that: UInt): UInt = {
+    operand_A := that.data
   }
-  override def +(b: UInt): UInt = {
+  override def +(that: UInt): UInt = {
     val operand_B = chisel3.UInt(fixed_width)
-    operand_B := b
-    output := operand_A + b
+    operand_B := that.data
+    output := operand_A + operand_B
     output
   }
-  override def -(b: UInt): UInt = {
+  override def -(that: UInt): UInt = {
     val operand_B = chisel3.UInt(fixed_width)
-    operand_B := b
+    operand_B := that.data
     output := operand_A - operand_B
     output
   }
-  override def *(b: UInt): UInt = {
+  override def *(that: UInt): UInt = {
     val operand_B = chisel3.UInt(fixed_width)
-    operand_B := b
+    operand_B := that.data
     output := operand_A * operand_B
     output
   }
-  override def /(b: UInt): UInt = {
+  override def /(that: UInt): UInt = {
+    val operand_B = chisel3.UInt(fixed_width)
+    operand_B := that.data
     output := operand_A / operand_B
     output
   }
   override def <<(b: UInt): UInt = {
-    output := operand_A << operand_B
+    output := operand_A << that.data
     output
   }
-  override def >>(b: UInt): UInt = {
-    output := operand_A >> operand_B
+  override def >>(that: UInt): UInt = {
+    output := operand_A >> that.data
     output
   }
   override def zero: UInt = 0.U
@@ -90,6 +93,101 @@ class SInt(val fixed_width: Width) extends GenericNumberType[SInt]:
     output
   }
   override def zero: SInt = 0.S
+
+class Float(val expWidth: Int, val sigWidth: Int) extends GenericNumberType[Float] {
+  val sign = Bool()
+  val exp = UInt(expWidth.W - 1)
+  val sig = UInt(sigWidth.W)
+
+  def :=(that: Float): Unit = {
+    sign := that.sign
+    exp := that.exp
+    sig := that.sig
+    val this_raw = rawFloatFromFN(expWidth, sigWidth, this.inBits)
+  }
+
+  def inBits(): Bits = {
+    val bits = Wire(Bits((expWidth + sigWidth).W))
+    bits := Cat(sign, exp, sig)
+    bits
+  }
+
+  def +(that: Float): Float = {
+    val that_raw = rawFloatFromFN(expWidth, sigWidth, that.inBits)
+    val output = Wire(new Float(exp_width, sig_width))
+    val adder = chisel3.Module(new AddRawFN(exp_width, sig_width))
+    adder.io.a := this_raw
+    adder.io.b := that_raw
+    val out_raw = adder.io.rawOut
+    output.sign := out_raw.sign
+    output.exp := out_raw.sExp.asUInt
+    output.sig := out_raw.sig(out_raw.expWidth + 1, out_raw.expWidth + 2 - sig_width + 1)
+    output
+  }
+
+  def -(that: Float): Float = {
+    val that_raw = rawFloatFromFN(expWidth, sigWidth, that.inBits)
+    val output = Wire(new Float(exp_width, sig_width))
+    val adder = chisel3.Module(new AddRawFN(exp_width, sig_width))
+    adder.io.subOp := 1.U
+    adder.io.a := this.this_raw
+    adder.io.b := that_raw
+    val out_raw = adder.io.rawOut
+    output.sign := out_raw.sign
+    output.exp := out_raw.sExp.asUInt
+    output.sig := out_raw.sig(out_raw.expWidth + 1, out_raw.expWidth + 2 - sig_width + 1)
+    output
+  }
+
+  def *(that: Float): Float = {
+    val that_raw = rawFloatFromFN(expWidth, sigWidth, that.inBits)
+    val output = Wire(new Float(exp_width, sig_width))
+    val multiplier = chisel3.Module(new MulRawFN(exp_width, sig_width, 0))
+    multiplier.io.a := this.this_raw
+    multiplier.io.b := that_raw
+    val out_raw = multiplier.io.rawOut
+    output.sign := out_raw.sign
+    output.exp := out_raw.sExp.asUInt
+    output.sig := out_raw.sig(out_raw.expWidth + 1, out_raw.expWidth + 2 - sig_width + 1)
+    output
+  }
+
+  def /(that: Float): Float = {
+    val that_raw = rawFloatFromFN(expWidth, sigWidth, that.inBits)
+    val output = Wire(new Float(exp_width, sig_width))
+    val divider = chisel3.Module(new DivRawFN(exp_width, sig_width, 0))
+    divider.io.a := this.this_raw
+    divider.io.b := that_raw
+    val out_raw = divider.io.rawOut
+    output.sign := out_raw.sign
+    output.exp := out_raw.sExp.asUInt
+    output.sig := out_raw.sig(out_raw.expWidth + 1, out_raw.expWidth + 2 - sig_width + 1)
+    output
+  }
+
+  def <<(that: UInt): Float = {
+    val output = Wire(new Float(exp_width, sig_width))
+    output := this
+    output.sig := output.sig << that
+    output
+  }
+
+  def >>(that: UInt): Float = {
+    val output = Wire(new Float(exp_width, sig_width))
+    output := this
+    output.sig := output.sig >> that
+    output
+  }
+
+  def zero: Float = {
+    val output = Wire(new Float(exp_width, sig_width))
+    output.sign := 0.U
+    output.exp := 0.U
+    output.sig := 0.U
+    output
+  }
+
+}
 
 class GenericALU[T](val width: Width) extends chisel3.Module {
   val io = IO(new Bundle {
